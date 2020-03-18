caa3869369b75cf9411a868dd4ddbaf4b3617e01
Hedgy134117 <hedgy134117@gmail.com>
2020-03-17 20:44:23 -0400

    new admin page

--- tamriel_trees/tamriel_trees/views.py
+++ tamriel_trees/tamriel_trees/views.py
@@ -3,12 +3,20 @@ from trees.models import Tree
 
 def homepage(request):
     allTrees = Tree.objects.all()
+    allTreesNames = { }
+    for tree in allTrees:
+        if tree.user in allTreesNames:
+            allTreesNames[tree.user].append(tree)
+        else:
+            allTreesNames[tree.user] = [tree]
     trees = []
     for tree in allTrees:
         if tree.user == request.user:
             trees.append(tree)
 
+    print(allTreesNames)
+
     if request.user.is_superuser:
-        return render(request, 'homepage.html', {'trees': allTrees})
+        return render(request, 'homepage.html', {'trees': allTreesNames, 'admin': True})
     else:
-        return render(request, 'homepage.html', {'trees': trees})
+        return render(request, 'homepage.html', {'trees': trees, 'admin': False})
